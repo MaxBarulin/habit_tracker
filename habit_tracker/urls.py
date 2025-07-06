261ef525d807295547dfd43c03da155e3d3df3bf
Max Barulin <barulin.max@gmail.com>
2025-07-06 21:22:15 +0300

    add urls project

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.routers import DefaultRouter
 
-from users.apps import UsersConfig
-from users.views import UserCreateAPIView, UserRetrieveAPIView
+from habit_tracker.apps import HabitTrackerConfig
+from habit_tracker.views import HabitViewSet, PublishHabitAPIView
 
-app_name = UsersConfig.name
+app_name = HabitTrackerConfig.name
+
+router = DefaultRouter()
+router.register(prefix=r"habit", viewset=HabitViewSet, basename='habit')
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user'),
-
-    path('token/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='token'),
-    path('token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='token_refresh'),
-]
+    path('publish_habit_list/', PublishHabitAPIView.as_view(permission_classes=[IsAuthenticated]),
+         name='publish_habit_list'),
+] + router.urls
